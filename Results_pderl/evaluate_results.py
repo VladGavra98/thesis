0153992b0732b79a435d99717bb907447677cb9d
vladg <vladgavra98@gmail.com>
2022-04-12 13:02:20 +0200

    Added pderl manually

---
+++ Results_pderl/evaluate_results.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import os
+# plt.updatercParams({'fontSize:16'})
+
+logs = 'pderl/logs'
+ddpg_score = np.genfromtxt(logs + '/ddpg_score.csv', skip_header= 1, delimiter=',')
+erl_score = np.genfromtxt(logs + '/erl_score.csv', skip_header= 1, delimiter=',')
+
+fig,ax = plt.subplots()
+fig.suptitle("Average Reward for 'Swimmer v3'")
+ax.plot(ddpg_score[:,0], ddpg_score[:,1], label = 'DDPG')
+ax.plot(ddpg_score[:,0], erl_score[:,1], label = 'PD-ERL')
+ax.set_ylabel("Average Reward [-]")
+ax.set_xlabel("Epochs [frames]")
+ax.legend(loc= 'best')
+fig.tight_layout()
+plt.show()
