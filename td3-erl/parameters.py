a351db0225689c860fc12a449f14ad2359930b96
vladg <vladgavra98@gmail.com>
2022-07-13 11:38:56 +0200

    prepared for train on workstation

--- td3-erl/parameters.py
+++ td3-erl/parameters.py
@@ -49,7 +49,7 @@ class Parameters:
         self.seed  = cla.seed
         self.batch_size = 128
         self.use_done_mask = True
-        self.buffer_size = 40_000         
+        self.buffer_size = 200_000         
         self.noise_sd = 0.2
         self.use_ounoise = cla.use_ounoise
 
